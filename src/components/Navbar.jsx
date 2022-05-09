05cc38dbb581c0f89121371092675317fb687027
samy-2002 <sagar.dw20@gmail.com>
2022-05-10 00:27:14 +0530

    Oprimazitation in Mobile nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -102,7 +102,7 @@ const Navbar = () => {
                                     src={TodoIcon}
                                     alt=""
                                 />
-                                <p className="text-lg md:text-base">
+                                <p className="text-base">
                                     Todo List
                                 </p>
                             </li>
@@ -112,7 +112,7 @@ const Navbar = () => {
                                     src={CalendarIcon}
                                     alt=""
                                 />
-                                <span className="text-lg md:text-base">
+                                <span className="text-base">
                                     Calendar
                                 </span>
                             </li>
@@ -122,7 +122,7 @@ const Navbar = () => {
                                     src={ReminderIcon}
                                     alt=""
                                 />
-                                <span className="text-lg md:text-base">
+                                <span className="text-base">
                                     Reminder
                                 </span>
                             </li>
@@ -132,7 +132,7 @@ const Navbar = () => {
                                     src={Planicon}
                                     alt=""
                                 />
-                                <span className="text-lg md:text-base">
+                                <span className="text-base">
                                     Planning
                                 </span>
                             </li>
@@ -143,7 +143,7 @@ const Navbar = () => {
                             onClick={showDrop2}
                             className="relative flex cursor-pointer flex-row items-center text-base text-[#696969] hover:text-[#141414] "
                         >
-                            <span className="mr-4 text-lg md:mr-2">
+                            <span className="mr-4 mr-2">
                                 Company
                             </span>
                             <img
@@ -162,17 +162,17 @@ const Navbar = () => {
                             className="drop2 md:shadowCustom hidden h-max w-max space-y-3 rounded-lg bg-white py-2 px-4 md:absolute md:mt-8 md:py-4 md:px-8"
                         >
                             <li className="cursor-pointer text-[#696969]">
-                                <span className="text-lg md:text-base">
+                                <span className="text-base">
                                     History
                                 </span>
                             </li>
                             <li className="cursor-pointer text-[#696969]">
-                                <span className="text-lg md:text-base">
+                                <span className="text-base">
                                     Our Team
                                 </span>
                             </li>
                             <li className="cursor-pointer text-[#696969]">
-                                <span className="text-lg md:text-base">
+                                <span className="text-base">
                                     Blog
                                 </span>
                             </li>
